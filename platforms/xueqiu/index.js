3d4585ef0145d37d81ab938feefca2ff4045b44e
realwugang <realwugang@aliyun.com>
2018-12-21 20:24:30 +0800

    修复雪球cookie失效

--- platforms/xueqiu/index.js
+++ platforms/xueqiu/index.js
@@ -1,9 +1,18 @@
 const request = require("superagent");
+let cookie = "";
+function reloadHomePage(){
+    request.get(`https://xueqiu.com/`).end((err, res)=>{
+        cookie = res.headers["set-cookie"].join(";")
+    })
+}
 
 module.exports = function (callback) {
     request.get(`https://xueqiu.com/v4/statuses/public_timeline_by_category.json?since_id=-1&max_id=-1&count=10&category=105`)
-        .set("cookie", "aliyungf_tc=AQAAAO2AvgtndQUAkMzUdvCjgHpYM/wK; xq_a_token=a9b845d5970a83e27be65c93bdf924687f8371d2; xq_a_token.sig=CoFzisEpUBVZUXIH80jgYl0aGeY; xq_r_token=5301599ba625f213b3df384282750cb9d40cbe95; xq_r_token.sig=bGXk935CYJh5r9TyOJSQP09JOHU; u=941545314143123; Hm_lvt_1db88642e346389874251b5a1eded6e3=1545314143; device_id=c41c50917b23c1d9391899cb9b65a9b5; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1545314637")
+        .set("cookie", cookie)
         .end((err, res) => {
+            if(res.body.error_code){
+               return reloadHomePage();
+            }
             const list = res.body && res.body.list || [];
             callback(list.map(item => {
                 const _item = JSON.parse(item.data)
